5f9fd8d072f0530745919493583ea92cccdf1fbd
Ivars Knets <ivars.knets@gmail.com>
2022-11-04 11:38:22 +0200

    teskset 3

---
+++ api/src/router.js
@@ -0,0 +1,34 @@
+
+const express = require("express");
+const multer = require("multer");
+
+const router = express.Router();
+const storage = multer.diskStorage({ destination: "api/uploads/", filename });
+const upload = multer({ fileFilter, storage });
+
+
+function filename( req, file, callback ){
+ callback(null, file.originalname);
+}
+
+function fileFilter(req, file, callback ){
+    if( file.mimetype !== 'image/png' ){
+        req.fileValidationError = 'Wrong file type';
+        callback(null, false, new Error('Wrong file type') );
+    } else {
+        callback( null, true);
+    }
+}
+
+
+
+router.post( '/upload', upload.single('photo'), (req,res)=>{
+    if(req.fileValidationError){
+        res.status(400).json({error: req.fileValidationError});
+    } else {
+        res.status(201).json({success: true});
+    }
+});
+
+
+module.exports = router;
