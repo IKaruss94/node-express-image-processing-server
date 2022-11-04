82cce06c2648cf77d18c4c82c6f12d57502b49df
Ivars Knets <ivars.knets@gmail.com>
2022-11-04 14:10:15 +0200

    taskset 5

--- api/src/router.js
+++ api/src/router.js
@@ -2,6 +2,7 @@
 const {Router} = require("express");
 const multer = require("multer");
 const path = require("path");
+const imageProcessor = require("./imageProcessor");
 
 const router = Router();
 const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
@@ -26,12 +27,17 @@ function fileFilter(req, file, callback ){
 
 
 
-router.post( '/upload', upload.single('photo'), (req,res)=>{
-    if(req.fileValidationError){
-        res.status(400).json({error: req.fileValidationError});
-    } else {
-        res.status(201).json({success: true});
+router.post( '/upload', upload.single('photo'), async function(req,res){
+    if(req.fileValidationError) return res.status(400).json({error: req.fileValidationError});
+      
+    try {
+        await imageProcessor(req.file.filename);
+    } catch (error) {
+        
     }
+  
+    return res.status(201).json({success: true});
+    
 });
 
 router.get('/photo-viewer',(req, res) => {
